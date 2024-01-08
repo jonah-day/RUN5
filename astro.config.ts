d41b570399bd76e1f3fc30110ca669773271b749
jonah-day <jonahday15@gmail.com>
2024-01-08 02:08:13 -0700

    prep for deployment

--- astro.config.ts
+++ astro.config.ts
@@ -12,15 +12,15 @@ const {
 } = loadEnv(import.meta.env.MODE, process.cwd(), '');
 
 // Different environments use different variables
-const projectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
-const dataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
+const thisProjectId = PUBLIC_SANITY_STUDIO_PROJECT_ID || PUBLIC_SANITY_PROJECT_ID;
+const thisDataset = PUBLIC_SANITY_STUDIO_DATASET || PUBLIC_SANITY_DATASET;
 
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
-    projectId,
-    dataset,
+    projectId: thisProjectId,
+    dataset: thisDataset,
     apiVersion: `2022-03-07`,
     studioBasePath: '/admin',
     //`false` if you want to ensure fresh data / Set useCdn to false if you're building statically
