e74facf1d1275fc230ddef9cd980af22450e40bc
jonah-day <jonahday15@gmail.com>
2024-01-19 21:48:40 -0700

    updated landscape

--- src/lib/landscape.js
+++ src/lib/landscape.js
@@ -1,2 +1,28 @@
 
-let layerArr = document.querySelectorAll(".layer");
+
+let layerArr = document.querySelectorAll(".landscape");
+let layersObj = {};
+
+layerArr.forEach((element) => {
+    let elementId = element.id;
+    layersObj[elementId] = element;
+});
+
+// for (let each of layerArr){
+//   let elementId = each.id;
+//   layersObj[each] = elementId;
+// }
+
+function landscape(){
+
+  let scrollDist = document.documentElement.scrollTop;
+
+  for (let each in layersObj){
+    let offSet = each;
+    let newPos = offSet * scrollDist;
+
+    layersObj[each].style.marginTop = `-${newPos}px`;
+  }
+}
+
+window.addEventListener("scroll", landscape);
