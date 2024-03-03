90b3ae116eaa3a500262fbc8559293c4795f1f4d
jonah-day <jonahday25@gmail.com>
2024-03-02 21:20:59 -0700

    added dynamic copyright thing

---
+++ .vercel/output/static/_astro/hoisted.FauSupHr.js
@@ -0,0 +1,1 @@
+import"./hoisted.9Nv1T8Zi.js";import"./index.PDhEm6KS.js";document.getElementById("copyright").textContent=new Date().getFullYear().toString();
--- .vercel/output/static/_astro/hoisted.toDFC-7m.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.9Nv1T8Zi.js";import"./index.PDhEm6KS.js";
